4f0c10b3d2525aa18d08f5745f9785efbf44fe18
muchiriprojects <muchiriprojects@gmail.com>
2022-10-01 11:48:25 +0300

    edited forms

--- name.js
+++ name.js
@@ -26,7 +26,6 @@ var birthday ="null";
    var day = parseInt(((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7).toFixed(0)));
 
     
-
 // use switch case expression to get Akhan name.
 var x = day;
 var y = 0;
